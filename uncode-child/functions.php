a55fb6b5b5cd2690f5ca90b4311e2aefce1d7031
dgamoni <dgamoni@gmail.com>
2025-10-28 10:12:17 +0100

    pull

--- uncode-child/functions.php
+++ uncode-child/functions.php
@@ -108,4 +108,29 @@ function carregar_scripts_mapa_autonomo() { // O nome da função está aqui
 add_action( 'wp_enqueue_scripts', 'carregar_scripts_mapa_autonomo' ); 
 
 // --- FIM DA SOLUÇÃO ---
+// 
+// 
+function add_meta_pixel_to_head() {
+    ?>
+    
 
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1379220637059571');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1379220637059571&ev=PageView&noscript=1"
+/></noscript>
+
+
+    <?php
+}
+add_action( 'wp_head', 'add_meta_pixel_to_head' );
